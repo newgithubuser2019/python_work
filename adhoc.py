68b414247dd34e2cb115e191c7a87bf73272feb5
Dmitry Potapov <potapov_d@list.ru>
2023-07-29 21:32:46 +0300

    insignificant changes

--- adhoc.py
+++ adhoc.py
@@ -11,7 +11,7 @@ from functools import reduce
 import numpy as np
 import openpyxl
 import pandas as pd
-import sidetable
+# import sidetable
 from pandas.tseries.offsets import DateOffset
 
 import функции
@@ -56,29 +56,3 @@ findf = pd.DataFrame()
 filename0 = USERPROFILE + "\\Documents\\Google Sheets Test.xlsx"
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-"""
-SHEET_ID = '1Y56CvHeHegMYlx1V-bzC9jc7cqRf7tLgRcIvRKbn5sk'
-SHEET_NAME = 'Subscriptions'
-url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
-#df = pd.read_excel(url, header=0, usecols="A,B,C,D", engine="openpyxl")
-df = pd.read_csv(url, header=0)
-print(df.head())
-df = df["Full Name", "Amount"]
-print(df.head())
-"""
-df = pd.read_excel(
-                filename0,
-                sheet_name="Subscriptions",
-                index_col=None,
-                engine = "openpyxl",
-                header=0,
-                usecols = "A:G",
-                #dtype = {"I": str},
-                )
-df["First Name"] = df["Full Name"].str.rsplit(" ").str[0]
-df["Last Name"] = df["Full Name"].str.rsplit(" ").str[-1]
-print(df.head())
-#df_active = df.drop(df.loc[df["Status"]=="Inactive"].index)
-df_active = df.loc[df["Status"]=="Active"]
-print(df_active)

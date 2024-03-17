cba14e33981c1f1f96caf866d6f3232462e3b2d8
Dmitry Potapov <potapov_d@list.ru>
2024-03-17 12:53:08 +0300

    добавил логику для уровня детализации сравнения

--- kky_sravnenie.py
+++ kky_sravnenie.py
@@ -42,10 +42,12 @@ findf = pd.DataFrame()
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # prompts for user input
 prompt0 = "\nцб или старка?: "
+prompt1 = "\nдетализация до корпусов?: "
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # user inputs
 inp0 = input(prompt0)
+inp1 = input(prompt1)
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # file paths
@@ -56,10 +58,10 @@ filename4 = USERPROFILE + "\\Documents\\Работа\\отчетность\\еж
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # накопительный отчет - сравнение
-дата_меньше = "01.03.2024"
-дата_меньше = pd.to_datetime(дата_меньше, dayfirst=True)
-дата_больше = "16.03.2024"
-дата_больше = pd.to_datetime(дата_больше, dayfirst=True)
+дата_нач = "01.03.2024"
+дата_нач = pd.to_datetime(дата_нач, dayfirst=True)
+дата_кон = "16.03.2024"
+дата_кон = pd.to_datetime(дата_кон, dayfirst=True)
 
 # ЦБ-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 if inp0 == "цб":
@@ -87,8 +89,8 @@ if inp0 == "цб":
                 })
         df_цб = df_цб.dropna(subset=["ОП"])
         df_цб["д_сдачи"] = pd.to_datetime(df_цб["д_сдачи"], dayfirst=True)
-        df_цб = df_цб.drop(df_цб[(df_цб["д_сдачи"] < дата_меньше)].index)
-        df_цб = df_цб.drop(df_цб[(df_цб["д_сдачи"] > дата_больше)].index)
+        df_цб = df_цб.drop(df_цб[(df_цб["д_сдачи"] < дата_нач)].index)
+        df_цб = df_цб.drop(df_цб[(df_цб["д_сдачи"] > дата_кон)].index)
         df_цб["выбр_г"] = df_цб["выбр_г"].fillna(0)
         df_цб["выбр_в"] = df_цб["выбр_в"].fillna(0)
         # print("\ndf_цб")
@@ -97,9 +99,15 @@ if inp0 == "цб":
         df_цб["гол"] = df_цб["гол"] - df_цб["выбр_г"]
         df_цб["вес"] = df_цб["вес"] - df_цб["выбр_в"]
         df_цб = df_цб.drop(["выбр_г", "выбр_в"], axis = 1)
-        df_цб = df_цб.groupby(["ОП", "корп", "д_сдачи"], as_index=False).agg({"гол": "sum", "вес": "sum"})
-        df_цб.loc[df_цб["ОП"].str.contains("Муром"), ["корп"]] = df_цб["корп"]*10
-        df_цб = df_цб.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+        if inp1 == "да" or inp1 == "yes" or inp1 == "y":
+                df_цб = df_цб.groupby(["д_сдачи", "ОП", "корп"], as_index=False).agg({"гол": "sum", "вес": "sum"})
+                df_цб.loc[df_цб["ОП"].str.contains("Муром"), ["корп"]] = df_цб["корп"]*10
+        if inp1 == "нет" or inp1 == "no" or inp1 == "n":
+                df_цб = df_цб.groupby(["д_сдачи", "ОП"], as_index=False).agg({"гол": "sum", "вес": "sum"})
+        try:
+                df_цб = df_цб.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+        except KeyError:
+                df_цб = df_цб.sort_values(by=["д_сдачи", "ОП"], ascending=True)
         df_цб.reset_index(inplace = True)
         df_цб = df_цб.drop(["index"], axis = 1)
         print("\ndf_цб")
@@ -131,8 +139,8 @@ if inp0 == "цб":
                 place="After"
                 )
         df_поступление["д_сдачи"] = pd.to_datetime(df_поступление["д_сдачи"], dayfirst=True)
-        df_поступление = df_поступление.drop(df_поступление[(df_поступление["д_сдачи"] < дата_меньше)].index)
-        df_поступление = df_поступление.drop(df_поступление[(df_поступление["д_сдачи"] > дата_больше)].index)
+        df_поступление = df_поступление.drop(df_поступление[(df_поступление["д_сдачи"] < дата_нач)].index)
+        df_поступление = df_поступление.drop(df_поступление[(df_поступление["д_сдачи"] > дата_кон)].index)
         df_поступление.loc[df_поступление["ОП"].str.contains("Агрин"), ["ОП"]] = "Агрин"
         df_поступление.loc[df_поступление["ОП"].str.contains("Коренская"), ["ОП"]] = "Коренское"
         df_поступление.loc[df_поступление["ОП"].str.contains("Графовская"), ["ОП"]] = "Графовское"
@@ -146,9 +154,15 @@ if inp0 == "цб":
         df_поступление.loc[df_поступление["ОП"].str.contains("Рождественская"), ["ОП"]] = "Рождественское"
         df_поступление.loc[df_поступление["корп"].str.contains("корпус"), ["корп"]] = df_поступление["корп"].str[0:3]
         df_поступление.loc[df_поступление["корп"].str.contains(" к", na=False), ["корп"]] = df_поступление["корп"].str[0:2]
-        df_поступление = df_поступление.groupby(["ОП", "корп", "д_сдачи"], as_index=False).agg({"ттн_гол": "sum", "ттн_вес": "sum"})
-        df_поступление["корп"] = pd.to_numeric(df_поступление["корп"], errors="coerce")
-        df_поступление = df_поступление.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+        if inp1 == "да" or inp1 == "yes" or inp1 == "y":
+                df_поступление = df_поступление.groupby(["д_сдачи", "ОП", "корп"], as_index=False).agg({"ттн_гол": "sum", "ттн_вес": "sum"})
+                df_поступление["корп"] = pd.to_numeric(df_поступление["корп"], errors="coerce")
+        if inp1 == "нет" or inp1 == "no" or inp1 == "n":
+                df_поступление = df_поступление.groupby(["д_сдачи", "ОП"], as_index=False).agg({"ттн_гол": "sum", "ттн_вес": "sum"})
+        try:
+                df_поступление = df_поступление.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+        except KeyError:
+                df_поступление = df_поступление.sort_values(by=["д_сдачи", "ОП"], ascending=True)
         df_поступление.reset_index(inplace = True)
         df_поступление = df_поступление.drop(["index"], axis = 1)
         print("\ndf_поступление")
@@ -160,13 +174,18 @@ if inp0 == "цб":
         # sys.exit()
 
         # merging dataframes---------------------------------------------------------------------------------------------------------------------------------------------------
-        df_цб = pd.merge(df_цб, df_поступление, how = "outer", on = ["ОП", "корп", "д_сдачи"])
-        df_цб = df_цб.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
-        df_цб["разн_гол"] = df_цб["гол"] - df_цб["ттн_гол"]
-        df_цб["разн_вес"] = df_цб["вес"] - df_цб["ттн_вес"]
-        df_цб = df_цб.drop(df_цб[(df_цб["разн_гол"] == 0) & (df_цб["разн_вес"] == 0)].index)
-        df_цб.loc[df_цб["разн_вес"] < 0, ["разн_вес"]] = df_цб["разн_вес"]*(-1) # зачем я умножаю?
-        df_цб = df_цб.drop(df_цб[(df_цб["разн_гол"] == 0) & (df_цб["разн_вес"] < 0.000000001)].index)
+        try:
+                df_цб = pd.merge(df_цб, df_поступление, how = "outer", on = ["д_сдачи", "ОП", "корп"])
+                df_цб = df_цб.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+        except KeyError:
+                df_цб = pd.merge(df_цб, df_поступление, how = "outer", on = ["д_сдачи", "ОП"])
+                df_цб = df_цб.sort_values(by=["д_сдачи", "ОП"], ascending=True)
+        # df_цб = df_цб.sort_values(by=["д_сдачи", "ОП"], ascending=True)
+        df_цб["НО-1С_гол"] = df_цб["гол"] - df_цб["ттн_гол"]
+        df_цб["НО-1С_вес"] = df_цб["вес"] - df_цб["ттн_вес"]
+        df_цб = df_цб.drop(df_цб[(df_цб["НО-1С_гол"] == 0) & (df_цб["НО-1С_вес"] == 0)].index)
+        # df_цб.loc[df_цб["НО-1С_вес"] < 0, ["НО-1С_вес"]] = df_цб["НО-1С_вес"]*(-1) # зачем я умножаю?
+        df_цб = df_цб.drop(df_цб[(df_цб["НО-1С_гол"] == 0) & (df_цб["НО-1С_вес"] < 0.000000001)].index)
         функции.print_line("hyphens")
         print("\nСРАВНЕНИЕ")
         if df_цб.empty == True:
@@ -200,21 +219,27 @@ if inp0 == "старка":
         df_starka.loc[df_starka["ОП"].str.contains("Разуменская"), ["ОП"]] = "Разуменская"
         df_starka.loc[df_starka["ОП"].str.contains("Тихая сосна"), ["ОП"]] = "Тихая сосна"
         df_starka["д_сдачи"] = pd.to_datetime(df_starka["д_сдачи"], dayfirst=True)
-        df_starka = df_starka.drop(df_starka[(df_starka["д_сдачи"] < дата_меньше)].index)
-        df_starka = df_starka.drop(df_starka[(df_starka["д_сдачи"] > дата_больше)].index)
-        df_starka = df_starka.groupby(["ОП", "корп", "д_сдачи"], as_index=False).agg({"гол": "sum", "вес": "sum"})
-        df_starka = df_starka.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+        df_starka = df_starka.drop(df_starka[(df_starka["д_сдачи"] < дата_нач)].index)
+        df_starka = df_starka.drop(df_starka[(df_starka["д_сдачи"] > дата_кон)].index)
+        if inp1 == "да" or inp1 == "yes" or inp1 == "y":
+                df_starka = df_starka.groupby(["д_сдачи", "ОП", "корп"], as_index=False).agg({"гол": "sum", "вес": "sum"})
+        if inp1 == "нет" or inp1 == "no" or inp1 == "n":
+                df_starka = df_starka.groupby(["д_сдачи", "ОП"], as_index=False).agg({"гол": "sum", "вес": "sum"})
+        try:
+                df_starka = df_starka.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+                #
+                df_starka["корп"] = df_starka["корп"].astype(str)+"_"
+                df_starka["корп"] = "_"+df_starka["корп"]
+                # df_starka["корп"] = df_starka["корп"].map(lambda x: x.rstrip(" ")) # здесь не пробел, а специальный символ из 1С
+                # df_starka["корп"] = df_starka["корп"].str.replace(" ","") # здесь не пробел, а специальный символ из 1С
+                df_starka["корп"] = df_starka["корп"].apply(lambda x: x.replace(" ","")) # здесь не пробел, а специальный символ из 1С
+                df_starka["корп"] = df_starka["корп"].apply(lambda x: x.replace("_",""))
+                # df_starka["корп"] = df_starka["корп"].apply(lambda x: float(x) if str(x).isdigit() else x)
+                # df_starka["корп2"] = df_starka["корп"].dtype
+        except KeyError:
+                df_starka = df_starka.sort_values(by=["д_сдачи", "ОП"], ascending=True)
         df_starka.reset_index(inplace = True)
         df_starka = df_starka.drop(["index"], axis = 1)
-        #
-        df_starka["корп"] = df_starka["корп"].astype(str)+"_"
-        df_starka["корп"] = "_"+df_starka["корп"]
-        # df_starka["корп"] = df_starka["корп"].map(lambda x: x.rstrip(" ")) # здесь не пробел, а специальный символ из 1С
-        # df_starka["корп"] = df_starka["корп"].str.replace(" ","") # здесь не пробел, а специальный символ из 1С
-        df_starka["корп"] = df_starka["корп"].apply(lambda x: x.replace(" ","")) # здесь не пробел, а специальный символ из 1С
-        df_starka["корп"] = df_starka["корп"].apply(lambda x: x.replace("_",""))
-        # df_starka["корп"] = df_starka["корп"].apply(lambda x: float(x) if str(x).isdigit() else x)
-        # df_starka["корп2"] = df_starka["корп"].dtype
         #
         print("\ndf_starka")
         print(df_starka)
@@ -246,27 +271,33 @@ if inp0 == "старка":
                 place="After"
                 )
         df_поступление_starka["д_сдачи"] = pd.to_datetime(df_поступление_starka["д_сдачи"], dayfirst=True)
-        df_поступление_starka = df_поступление_starka.drop(df_поступление_starka[(df_поступление_starka["д_сдачи"] < дата_меньше)].index)
-        df_поступление_starka = df_поступление_starka.drop(df_поступление_starka[(df_поступление_starka["д_сдачи"] > дата_больше)].index)
+        df_поступление_starka = df_поступление_starka.drop(df_поступление_starka[(df_поступление_starka["д_сдачи"] < дата_нач)].index)
+        df_поступление_starka = df_поступление_starka.drop(df_поступление_starka[(df_поступление_starka["д_сдачи"] > дата_кон)].index)
         df_поступление_starka.loc[df_поступление_starka["ОП"].str.contains("Истобнянская"), ["ОП"]] = "Истобнянская"
         df_поступление_starka.loc[df_поступление_starka["ОП"].str.contains("Муромская"), ["ОП"]] = "Муромская"
         df_поступление_starka.loc[df_поступление_starka["ОП"].str.contains("Разуменская"), ["ОП"]] = "Разуменская"
         df_поступление_starka.loc[df_поступление_starka["ОП"].str.contains("Тихая сосна"), ["ОП"]] = "Тихая сосна"
         df_поступление_starka.loc[df_поступление_starka["корп"].str.contains("корпус"), ["корп"]] = df_поступление_starka["корп"].str[0:3]
         df_поступление_starka.loc[df_поступление_starka["корп"].str.contains(" к", na=False), ["корп"]] = df_поступление_starka["корп"].str[0:2]
-        df_поступление_starka = df_поступление_starka.groupby(["ОП", "корп", "д_сдачи"], as_index=False).agg({"ттн_гол": "sum", "ттн_вес": "sum"})
-        df_поступление_starka = df_поступление_starka.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+        if inp1 == "да" or inp1 == "yes" or inp1 == "y":
+                df_поступление_starka = df_поступление_starka.groupby(["д_сдачи", "ОП", "корп"], as_index=False).agg({"ттн_гол": "sum", "ттн_вес": "sum"})
+        if inp1 == "нет" or inp1 == "no" or inp1 == "n":
+                df_поступление_starka = df_поступление_starka.groupby(["д_сдачи", "ОП"], as_index=False).agg({"ттн_гол": "sum", "ттн_вес": "sum"})
+        try:
+                df_поступление_starka = df_поступление_starka.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+                #
+                df_поступление_starka["корп"] = df_поступление_starka["корп"].astype(str)+"_"
+                df_поступление_starka["корп"] = "_"+df_поступление_starka["корп"]
+                # df_поступление_starka["корп"] = df_поступление_starka["корп"].map(lambda x: x.rstrip(" ")) # здесь не пробел, а специальный символ из 1С
+                # df_поступление_starka["корп"] = df_поступление_starka["корп"].str.replace(" ","") # здесь не пробел, а специальный символ из 1С
+                df_поступление_starka["корп"] = df_поступление_starka["корп"].apply(lambda x: x.replace(" ","")) # здесь не пробел, а специальный символ из 1С
+                df_поступление_starka["корп"] = df_поступление_starka["корп"].apply(lambda x: x.replace("_",""))
+                # df_поступление_starka["корп"] = df_поступление_starka["корп"].apply(lambda x: float(x) if str(x).isdigit() else x)
+                # df_поступление_starka["корп2"] = df_поступление_starka["корп"].dtype
+        except KeyError:
+                df_поступление_starka = df_поступление_starka.sort_values(by=["д_сдачи", "ОП"], ascending=True)
         df_поступление_starka.reset_index(inplace = True)
         df_поступление_starka = df_поступление_starka.drop(["index"], axis = 1)
-        #
-        df_поступление_starka["корп"] = df_поступление_starka["корп"].astype(str)+"_"
-        df_поступление_starka["корп"] = "_"+df_поступление_starka["корп"]
-        # df_поступление_starka["корп"] = df_поступление_starka["корп"].map(lambda x: x.rstrip(" ")) # здесь не пробел, а специальный символ из 1С
-        # df_поступление_starka["корп"] = df_поступление_starka["корп"].str.replace(" ","") # здесь не пробел, а специальный символ из 1С
-        df_поступление_starka["корп"] = df_поступление_starka["корп"].apply(lambda x: x.replace(" ","")) # здесь не пробел, а специальный символ из 1С
-        df_поступление_starka["корп"] = df_поступление_starka["корп"].apply(lambda x: x.replace("_",""))
-        # df_поступление_starka["корп"] = df_поступление_starka["корп"].apply(lambda x: float(x) if str(x).isdigit() else x)
-        # df_поступление_starka["корп2"] = df_поступление_starka["корп"].dtype
         #
         print("\ndf_поступление_starka")
         print(df_поступление_starka)
@@ -277,13 +308,17 @@ if inp0 == "старка":
         print(вес_сумма)
 
         # merging dataframes---------------------------------------------------------------------------------------------------------------------------------------------------
-        df_starka = pd.merge(df_starka, df_поступление_starka, how = "outer", on = ["ОП", "корп", "д_сдачи"])
-        df_starka = df_starka.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
-        df_starka["разн_гол"] = df_starka["гол"] - df_starka["ттн_гол"]
-        df_starka["разн_вес"] = df_starka["вес"] - df_starka["ттн_вес"]
-        df_starka = df_starka.drop(df_starka[(df_starka["разн_гол"] == 0) & (df_starka["разн_вес"] == 0)].index)
-        df_starka.loc[df_starka["разн_вес"] < 0, ["разн_вес"]] = df_starka["разн_вес"]*(-1) # зачем я умножаю?
-        df_starka = df_starka.drop(df_starka[(df_starka["разн_гол"] == 0) & (df_starka["разн_вес"] < 0.000000001)].index)
+        try:
+                df_starka = pd.merge(df_starka, df_поступление_starka, how = "outer", on = ["д_сдачи", "ОП", "корп"])
+                df_starka = df_starka.sort_values(by=["д_сдачи", "ОП", "корп"], ascending=True)
+        except KeyError:
+                df_starka = pd.merge(df_starka, df_поступление_starka, how = "outer", on = ["д_сдачи", "ОП"])
+                df_starka = df_starka.sort_values(by=["д_сдачи", "ОП"], ascending=True)
+        df_starka["НО-1С_гол"] = df_starka["гол"] - df_starka["ттн_гол"]
+        df_starka["НО-1С_вес"] = df_starka["вес"] - df_starka["ттн_вес"]
+        df_starka = df_starka.drop(df_starka[(df_starka["НО-1С_гол"] == 0) & (df_starka["НО-1С_вес"] == 0)].index)
+        # df_starka.loc[df_starka["НО-1С_вес"] < 0, ["НО-1С_вес"]] = df_starka["НО-1С_вес"]*(-1) # зачем я умножаю?
+        df_starka = df_starka.drop(df_starka[(df_starka["НО-1С_гол"] == 0) & (df_starka["НО-1С_вес"] < 0.000000001)].index)
         функции.print_line("hyphens")
         print("\nСРАВНЕНИЕ")
         if df_starka.empty == True:
